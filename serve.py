cad29a20debfb64ddd89ac2a1172d8d8a6784f98
Paul Kendall <pkendall64@gmail.com>
2015-03-09 20:20:04 +1300

    Initial commit of polymer based mythtv controller

---
+++ serve.py
@@ -0,0 +1,24 @@
+# a truly minimal HTTP proxy
+
+import SocketServer
+import SimpleHTTPServer
+try:
+    from urllib.request import Request, urlopen  # Python 3
+except:
+    from urllib2 import Request, urlopen  # Python 2
+
+PORT = 8080
+
+class Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        print self.path
+        if(self.path.startswith("/proxy/")):
+            req = Request("http://"+self.path[7:])
+            req.add_header("Accept", "application/json")
+            self.copyfile(urlopen(req), self.wfile)
+        else:
+            SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+httpd = SocketServer.ForkingTCPServer(('', PORT), Proxy)
+print "serving at port", PORT
+httpd.serve_forever()
